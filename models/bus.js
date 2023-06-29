5663a964c1b2a76344a58a0f90607f9e513ffc34
John Mejia <jhonmejia573@gmail.com>
2023-06-29 10:34:36 -0500

    Checks en bus

--- models/bus.js
+++ models/bus.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 const busSchema = new mongoose.Schema({
-    cantidad_asientos: { type: String, require:true,  },
+    numero_bus: { type:Number, require:true },
+    cantidad_asientos: { type: String, require:true  },
     empresa_asignada: { type: String, require:true },
     createAT : {default: Date.now },
     estado:{type:Boolean, default:false}
--- routes/bus.js
+++ routes/bus.js
@@ -1,22 +1,28 @@
 import { Router } from "express"
 import httpBus from "../controllers/bus.js"
-import  {check} from "express-validator"
+import { check } from "express-validator"
 
 
-const router=new Router()
+const router = new Router()
 
-router.get('/bus',[
+router.get('/bus/:numero_bus', [
+], httpBus.getBus);
+
+router.post('/bus/agregar', [
+    check("numero_bus", "Numero del bus").not().isEmpty(),
     check("cantidad_asientos", "Asientos disponibles").not().isEmpty(),
     check("empresa_asignada", "Nombre de la empresa").not().isEmpty(),
-],httpBus.getBus);
+], httpBus.postBus);
 
-// router.get('/bus/:codigo',funcionesbus.getbusCodigo);
+router.post('/bus/buscar', [
+    check("numero_bus", "Número de bus requerido").not().isEmpty()
+], httpBus.postBuscarBus);
 
-// router.post('/agregar',funcionesbus.postAgregarbus);
+router.put('/bus/:numero_bus', [
+    check("cantidad_asientos", "Asientos disponibles requeridos").not().isEmpty()
+], httpBus.putEditar);
 
-// router.put('/bus/:codigo', funcionesbus.putEditarbus);
-
-// router.delete('/bus/:codigo', funcionesbus.deletebus);
+router.delete('/bus/:numero_bus', httpBus.deleteBus);
 
 export default router
 
