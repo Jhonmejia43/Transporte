202146fa9c9bb1ad336081a059c5a82c40ab0488
unknown <SENA@SGLDFPCATSD254.FormacionSena.RED>
2023-08-17 14:49:54 -0500

    Version con token

--- routes/ticket.js
+++ routes/ticket.js
@@ -2,45 +2,55 @@ import { Router } from "express"
 import httpTicket from "../controllers/ticket.js";
 import { check } from "express-validator";
 import validarCampos from "../middlewares/validar.js"
+import { validarJWT } from "../middlewares/validar-jwt.js";
 
 const router=new Router()
 
-router.get('/ticket', httpTicket.getTicket);
+router.get('/ticket', [
+    validarJWT
+],httpTicket.getTicket);
 
 router.get('/ticket/:id',[
+    validarJWT,
     check("id", "Digite el id").not().isEmpty(),
     check("id", "Digite el id").isMongoId(),
     validarCampos
 ],httpTicket.getTicketId);
 
 router.get('/tickets',[
+    validarJWT,
     check("fechaInicio", "Fecha inicio").not().isEmpty(),
     check("fechaFin", "Fecha fin").not().isEmpty(),
     validarCampos
 ], httpTicket.getTicketsPorFechas); // Nueva ruta para listar tickets en un rango de fechas
 
 router.get('/tickets/vendedor',[
+    validarJWT,
     check("vendedorId", "Digite el vendedor").not().isEmpty(),
     validarCampos
 ], httpTicket.getTicketsPorVendedor); // Nueva ruta para listar tickets vendidos por vendedor
 
 router.get('/tickets/ganancia',[
+    validarJWT,
     check("fechaInicio", "Fecha inicio").not().isEmpty(),
     check("fechaFin", "Fecha fin").not().isEmpty(),
     validarCampos
 ], httpTicket.getTotalGananciaPorFechas); // Nueva ruta para calcular la ganancia total
 
 router.get('/tickets/cliente', [
+    validarJWT,
     check("clienteId", "Digite el ID del cliente").not().isEmpty(),
     validarCampos
 ], httpTicket.getTicketsPorCliente); // Nueva ruta para listar tickets comprados por cliente
 
 router.get('/tickets/rutas', [
+    validarJWT,
     check("busId", "Digite el ID del bus").not().isEmpty(),
     validarCampos
 ], httpTicket.getRutasPorBus);
 
 router.get('/tickets/ganancia-ruta', [
+    validarJWT,
     check("rutaId", "Digite el ID de la ruta").not().isEmpty(),
     check("fechaInicio", "Fecha inicio").not().isEmpty(),
     check("fechaFin", "Fecha fin").not().isEmpty(),
@@ -49,6 +59,7 @@ router.get('/tickets/ganancia-ruta', [
 
 
 router.post('/agregar',[
+    validarJWT,
     check("vendedor_id", "Digite el id del vendedor").isMongoId(),
     check("cliente_id", "Digite el id del cliente").isMongoId(),
     check("ruta_id", "Digite el id de la ruta").isMongoId(),
@@ -58,17 +69,20 @@ router.post('/agregar',[
 ],httpTicket.postTicket);
 
 router.delete('/ticket/:id',[
+    validarJWT,
     check("id", "Digite el id").not().isEmpty(),
     check("id", "Digite el id").isMongoId(),
     validarCampos,
 ], httpTicket.deleteTicket);
 
 router.put('inactivarTicket/:id',[
+    validarJWT,
     check("id", "Digite el id").not().isEmpty(),
     check("id", "Digite el id").isMongoId(),
     validarCampos
 ],httpTicket.putTicketInactivar)
 router.put('activarTicket/:id',[
+    validarJWT,
     check("id", "Digite el id").not().isEmpty(),
     check("id", "Digite el id").isMongoId(),
     validarCampos
